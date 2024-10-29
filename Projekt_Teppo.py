51b4da7eb2541306486b492fa92122703ce9fe0f
LisbL <LisbethLepp12@gmail.com>
2024-10-29 16:16:26 +0200

    Uued taustad

--- Projekt_Teppo.py
+++ Projekt_Teppo.py
@@ -26,34 +26,49 @@ pygame.init()
 pygame.display.set_caption("Teppo reis koju")
 
 #Globaalsed muutujad
-BG_COLOR = (174, 242, 255)
-SCREEN_WIDTH, SCREEN_HEIGHT = 1100, 700
+SCREEN_WIDTH, SCREEN_HEIGHT = 320, 180
 FPS = 60
 PLAYER_SPEED = 5
-
+clock = pygame.time.Clock()
 WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+#mängu muutuja
+scroll = 0
+
+BG_images = []
+for i in range(1, 4):
+    BG_image = pygame.image.load(join("materjalid", "Taustad", "Free", "Jungle", f"{i}_layer.png" )).convert_alpha()
+    BG_images.append(BG_image)
+BG_width = BG_images[0].get_width()
 #Funktsioonid
-def get_BG(name):
-    image = pygame.image.load(join("materjalid", "Taustad", name))
-    _, _, width, height = image.get_rect()
-    tiles = []
+def draw_BG():
+    for x in range(3):
+        for i in BG_images:
+            WINDOW.blit((x*BG_width) + scroll, 0)#Pildid laevad üksteise peale
+
 
 #PÕHIPROGRAMM
 def main(window):
-    clock = pygame.time.Clock()
 
     #Mängu tsükkel
     running = True
     while running:
+        #Tausta laadimiseks
+        draw_BG()
 
         #Juhul kui tahame mängu sulgeda
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-
+        pygame.display.update()
         clock.tick(FPS)
+
+        #keypresses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            scroll -= 4
+        if key[pygame.K_RIGHT]:
+            scroll += 4
 
     pygame.quit()
 
