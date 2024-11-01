149eecab250f88050f2025f1e9137a1319e30b46
LisbL <LisbethLepp12@gmail.com>
2024-11-01 13:17:23 +0200

    Spritesheet moodul

--- Projekt_Teppo.py
+++ Projekt_Teppo.py
@@ -18,6 +18,7 @@ import pygame
 import os
 from os import listdir
 from os.path import isfile, join
+import spritesheet
 
 pygame.init()
 
@@ -44,6 +45,8 @@ for i in range(1, 4):
 BG_width = BG_images[0].get_width()
 #Sprite sheet muutujad
 sprite_sheet_image = pygame.image.load(join("materjalid", "Tegelased", "vita.png")).convert_alpha()
+#Instance
+sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
 BLACK = (0, 0, 0)
 
 #Funktsioonid
@@ -56,19 +59,13 @@ def draw_BG():
 def draw_ground():
     for x in range(15):
         WINDOW.blit(ground_image, ((x*ground_width) - scroll * 2.2, SCREEN_HEIGHT - ground_height))
-def get_image(sheet, frame, width, height, scale, colour):
-    image = pygame.Surface((width, height)).convert_alpha()
-    image.blit(sheet, (0, 0), ((frame*width), 0, width, height))
-    #Suuruse muutmiseks
-    image = pygame.transform.scale(image, (width * scale, height * scale))
-    image.set_colorkey(colour)
-    return image
+
 
 #Sprite frame
-frame_0 = get_image(sprite_sheet_image, 0, 24, 24, 2, BLACK)
-frame_1 = get_image(sprite_sheet_image, 1, 24, 24, 2, BLACK)
-frame_2 = get_image(sprite_sheet_image, 2, 24, 24, 2, BLACK)
-frame_3 = get_image(sprite_sheet_image, 3, 24, 24, 2, BLACK)
+frame_0 = sprite_sheet.get_image(0, 24, 24, 2, BLACK)
+frame_1 = sprite_sheet.get_image(1, 24, 24, 2, BLACK)
+frame_2 = sprite_sheet.get_image(2, 24, 24, 2, BLACK)
+frame_3 = sprite_sheet.get_image(3, 24, 24, 2, BLACK)
 
 #PÕHIPROGRAMM
 def main(window):
@@ -85,6 +82,8 @@ def main(window):
         WINDOW.blit(frame_1, (40,0))
         WINDOW.blit(frame_2, (80,0))
         WINDOW.blit(frame_3, (120,0))
+
+
         #Juhul kui tahame mängu sulgeda
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
