4e713fddd2743835673652335d162951b35f3604
Martti Virnas <marttivirnas@ut.ee>
2024-11-02 15:26:54 +0200

    Lisasin tekstile fondi

--- aken.py
+++ aken.py
@@ -6,7 +6,7 @@ screen = pygame.display.set_mode((800,600))
 screen.fill("darkblue")
 pygame.display.set_caption("Teppo Reis Koju")
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
+test_font = pygame.font.Font("fondid/Pixeltype.ttf", 75)
 
 test_surface = pygame.image.load("pildid/pixel-art-display-test.jpg")
 text_surface = test_font.render("Teppo Reis Koju", False, "White")
@@ -18,7 +18,7 @@ while True:
             exit()
 
     screen.blit(test_surface,(0,0)) #blit = block image transfer (one surface on another surface)
-    screen.blit(text_surface,(255,50))
+    screen.blit(text_surface,(215,50))
 
     pygame.display.update()
     clock.tick(60)
