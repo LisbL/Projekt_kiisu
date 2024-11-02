63ce0b27dd4fe9c9ab562b94a597dd1059dc0f26
Martti Virnas <marttivirnas@ut.ee>
2024-11-02 14:57:01 +0200

    Akna kuvamine

--- aken.py
+++ aken.py
@@ -1,2 +1,14 @@
-# kirjutasin midagi
-help miks ma ei näe seda vscode'is
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Teppo Reis Koju")
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
