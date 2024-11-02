73699c7228d0441821c8cdcda9a7f4c82f14b7d3
Martti Virnas <marttivirnas@ut.ee>
2024-11-02 15:16:07 +0200

    Aknale lisasin pildi

--- aken.py
+++ aken.py
@@ -3,8 +3,11 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
+screen.fill("darkblue")
 pygame.display.set_caption("Teppo Reis Koju")
 clock = pygame.time.Clock()
+
+test_surface = pygame.image.load("pildid/pixel-art-display-test.jpg")
 
 
 while True:
@@ -12,6 +15,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(test_surface,(0,0)) #blit = block image transfer (one surface on another surface)
 
     pygame.display.update()
     clock.tick(60)
