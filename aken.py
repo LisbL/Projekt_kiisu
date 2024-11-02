d30d2b64fc3fac53e3004d1ce4bff346b1737478
Martti Virnas <marttivirnas@ut.ee>
2024-11-02 16:44:54 +0200

    Lisasin kassi PNG pildi, mis liigub vasakult paremale lõpmatuseni. Vahetasin taustapilti.

--- aken.py
+++ aken.py
@@ -8,8 +8,13 @@ pygame.display.set_caption("Teppo Reis Koju")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("fondid/Pixeltype.ttf", 75)
 
-test_surface = pygame.image.load("pildid/pixel-art-display-test.jpg")
+test_surface = pygame.image.load("materjalid/Forest/Background.png").convert() # .convert() nii, et pygame suudaks lihtsamini töötada, mäng on kiirem
 text_surface = test_font.render("Teppo Reis Koju", False, "White")
+
+black_cat_surface = pygame.image.load("tegelased/Black cat/Cat-2-Sitting.png").convert_alpha()
+cat_image_size = (200, 200)
+black_cat_surface = pygame.transform.scale(black_cat_surface, cat_image_size)
+black_cat_x_pos = -150
 
 while True:
     for event in pygame.event.get():
@@ -17,8 +22,11 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface,(0,0)) #blit = block image transfer (one surface on another surface)
-    screen.blit(text_surface,(215,50))
+    screen.blit(test_surface,(0,-190)) #blit = block image transfer (one surface on another surface)
+    screen.blit(text_surface,(225,70))
+    black_cat_x_pos += 2
+    if black_cat_x_pos > 900: black_cat_x_pos = -150
+    screen.blit(black_cat_surface,(black_cat_x_pos,400))
 
     pygame.display.update()
     clock.tick(60)
