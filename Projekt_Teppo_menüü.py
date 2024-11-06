7b944250f7c1b8e37bf60cc04429aba64d7fc567
Martti Virnas <marttivirnas@ut.ee>
2024-11-06 17:00:41 +0200

    Lisasin menüüle taustapildi

--- Projekt_Teppo_menüü.py
+++ Projekt_Teppo_menüü.py
@@ -13,20 +13,22 @@ pygame.display.set_caption("Teppo reis koju")
 
 #define game variables
 start_game = False
-
+#background image
+BG = pygame.image.load("materjalid/dark forest/Preview.png").convert()
+BG = pygame.transform.scale(BG, (960,540))
 #button images
 start_img = pygame.image.load("pildid/Nupud/start_btn.png").convert_alpha()
 exit_img = pygame.image.load("pildid/Nupud/exit_btn.png").convert_alpha()
 
 #create buttons
-start_button = button.Button(screen_height // 2 + 210, screen_height // 2 + 30, start_img)
-exit_button = button.Button(screen_height // 2 + 210, screen_height // 2 + 180, exit_img)
+start_button = button.Button(screen_height // 2 + 205, screen_height // 2 + 30, start_img)
+exit_button = button.Button(screen_height // 2 + 205, screen_height // 2 + 180, exit_img)
 
 run = True
 while run:
 
     if start_game == False:
-        screen.fill((60, 60, 88))
+        screen.blit(BG, (0,0))
         if start_button.draw(screen):
             start_game = True
         if exit_button.draw(screen):
