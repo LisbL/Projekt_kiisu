fda402173c1b41b836d8e9f00f1e85303273025f
LisbL <LisbethLepp12@gmail.com>
2024-11-06 16:30:54 +0200

    Enne pööramise katsetamist

--- Projekt_Teppo2.py
+++ Projekt_Teppo2.py
@@ -11,16 +11,25 @@ BG = (50, 50, 50)
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Teppo reis koju")
 
+#Frame rate
+clock = pygame.time.Clock()
+FPS = 60
+
 #Player action variable
 moving_left = False
 moving_right = False
+
+
+def draw_BG():
+    screen.fill(BG)
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, x_player, y_player, w_player, h_player, speed):
         pygame.sprite.Sprite.__init__(self)
         self.speed = speed
-
+        self.direction = 1
+        self.flip = False
         self.player_rect = pygame.Rect(x_player, y_player, w_player, h_player)
         self.player_rect.center = (x, y)#Määrab rect objekti keskpunkti
 
@@ -31,8 +40,12 @@ class Player(pygame.sprite.Sprite):
         #assign movement variables if moving left or right
         if moving_left:
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if moving_right:
             dx = self.speed
+            self.flip = False
+            self.direction = 1
         
         #update rectangle position
         self.player_rect.x += dx
@@ -43,13 +56,16 @@ class Player(pygame.sprite.Sprite):
         pygame.draw.rect(screen, rect_colour, self.player_rect)
 
 
-player = Player(200, 200, 150, 400, 100, 50, 1)
+player = Player(200, 200, 150, 400, 100, 50, 5)
 
 colour = (3, 252, 227)
 
 running = True
 while running:
-    screen.fill(BG)
+
+    clock.tick(FPS)
+
+    draw_BG()
 
     player.move(moving_left, moving_right)
     player.draw(colour)
@@ -58,7 +74,8 @@ while running:
         #quit game
         if event.type == pygame.QUIT:
             running = False
-            #Pressed
+
+            #Key pressed
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 moving_left = True
@@ -66,12 +83,14 @@ while running:
                 moving_right = True
             if event.key == pygame.K_ESCAPE:
                 running = False
-            #Released
+            #Key released
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
                 moving_left = False
             if event.key == pygame.K_d:
                 moving_right = False
+
+        
     pygame.display.update()
 
 pygame.quit()
